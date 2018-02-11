739f935ade08f37238902758a5eee2d7d87911f8
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2018-02-11 14:28:33 +0200

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,1 +1,11 @@
 
+def bubbleSort(array) :
+  for m in range(0, len(array)) :
+    for i in range(1, len(array) - m) :
+      if array[i-1] > array[i] :
+        array[i-1], array[i] = array[i], array[i-1]
+  return array
+  
+arrayEx = [12,125,122,1200,4,143]
+res = bubbleSort(arrayEx)
+print(res)
